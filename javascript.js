7a190ee5874d0b5ebafe56f356b0ea096f500560
Maria <lrabin@gmail.com>
2019-04-29 16:49:23 -0400

    updated the current time

--- javascript.js
+++ javascript.js
@@ -1,4 +1,21 @@
 // Steps to complete:
+function startTime() {
+
+    var today = new Date();
+    var h = today.getHours();
+    //h = h > 12 ? h - 12 : h;
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date_time').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
 
 
 // 3. Create a way to retrieve employees from the employee database.
